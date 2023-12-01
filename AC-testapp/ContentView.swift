504b34c4eb83a3ba647ffeb96f633b2e2be2f0d7
Tyler Boston <tboston6789@gmail.com>
2023-11-30 18:40:26 -0600

    V0.1

--- AC-testapp/ContentView.swift
+++ AC-testapp/ContentView.swift
@@ -13,7 +13,41 @@ struct ContentView: View {
     @Query private var items: [Item]
 
     var body: some View {
+        
+        VStack {
+            // Pull in the image for the logo
+            Image("FenCo logo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding(.top, 10.0)
+            
+            // Pull in the image for the Bakers
+            Image("IMG_6406")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding(.top, 20.0)
+            
+            // Add a little space
+            Spacer()
+            
+            // Browse the Catalog
+            ZStack{
+                Image("browse button")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 300.0, height: 150.0)
+                Button(action: orderBiscuits) {
+                    Label("", systemImage: "browse button")
+                }
+                
+            }
+            .padding(.top, 40.0)
+            Spacer()
+        }
+        
+        /*
         NavigationSplitView {
+            
             List {
                 ForEach(items) { item in
                     NavigationLink {
@@ -36,10 +70,16 @@ struct ContentView: View {
             }
         } detail: {
             Text("Select an item")
-        }
+        }*/
     }
 
-    private func addItem() {
+    
+    private func orderBiscuits() {
+                    
+        }
+        
+    }
+   /* private func addItem() {
         withAnimation {
             let newItem = Item(timestamp: Date())
             modelContext.insert(newItem)
@@ -52,10 +92,87 @@ struct ContentView: View {
                 modelContext.delete(items[index])
             }
         }
+    }*/
+
+
+struct ContentViewTwo: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query private var items: [Item]
+    @State private var showConfirmation = false
+    
+    func addToCartConfirmation () {
+        
+        self.showConfirmation.toggle()
     }
+    var body: some View {
+        
+        
+        
+        VStack {
+            // Pull in the image for the Biscuit Button
+            Image("biscuit button")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding(.top, 10.0)
+                .onTapGesture {
+                    addToCartConfirmation()
+                }
+            
+           
+           //  Shows Order Confirmation 
+        }.alert("NOW WE'RE BAKING", isPresented: $showConfirmation){
+            Button("Add MO"){}
+        } message: {
+            Text("You've Now Added a Biscuit to Your Cart 😽")
+        }
+        
+        
+        
+        
+        
+    }
+        
+    
+    private func orderBiscuits() {
+        
+    
+        
+    }
+        /*
+        NavigationSplitView {
+            
+            List {
+                ForEach(items) { item in
+                    NavigationLink {
+                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
+                    } label: {
+                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
+                    }
+                }
+                .onDelete(perform: deleteItems)
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    EditButton()
+                }
+                ToolbarItem {
+                    Button(action: addItem) {
+                        Label("Add Item", systemImage: "plus")
+                    }
+                }
+            }
+        } detail: {
+            Text("Select an item")
+        }*/
+    }
+
+
+#Preview("Content View") {
+    ContentView()
+        .modelContainer(for: Item.self, inMemory: true)
 }
 
-#Preview {
-    ContentView()
+#Preview("Content View2") {
+    ContentViewTwo()
         .modelContainer(for: Item.self, inMemory: true)
 }
